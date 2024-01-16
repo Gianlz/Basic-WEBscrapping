86408bf2d77b0809eb41d844ae95651233d9b10f
Gianluca Zugno <67298422+Gianlz@users.noreply.github.com>
2024-01-16 01:29:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,13 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Search for the upcoming animes
 anchor_tags = soup.find_all("a", {"id": lambda x: x and x.startswith("#area")})
 anchor_texts = [tag.get_text() for tag in anchor_tags]
-lista = []
 
-# Iterate over the texts
-for text in anchor_texts:
-    lista.append(text)
 
 # Remove the blank spaces \n\n
-for i in lista:
+for i in anchor_texts:
     if i == "\n\n":
-        lista.remove(i)
+        anchor_texts.remove(i)
 
-lista = list(set(lista))
+lista = list(set(anchor_texts))
 print(lista)
     
